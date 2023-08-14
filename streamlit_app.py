9716169b672acf67680b12d6acb04e7aebdb917b
bkclaw113 <bkclaw113@gmail.com>
2023-08-14 07:09:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-From urllib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('MyParents New Healthy Diner')
 
